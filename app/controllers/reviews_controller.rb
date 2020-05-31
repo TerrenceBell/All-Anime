fd7057e8b50d1bf597f85e49d3df4aa1f91af8ce
TerrenceBell <terrencekbell@gmail.com>
2020-05-30 21:24:29 -0500

    created reviews model

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,50 @@
 class ReviewsController < ApplicationController
+
+    def index
+        @reviews = Review.all 
+    end
+
+    def show 
+        @review = Review.find(params[:id])
+    end
+
+    def new 
+        @review = Review.new 
+    end
+
+    def create
+        @review = Review.new(review_params)
+        if @review.save
+            redirect_to @review
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @review = Review.find(params[:id])
+    end
+
+    def update
+        @review = Review.find(params[:id])
+        @review.update(review_params)
+        if @review.save 
+            redirect_to @review 
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @review = Review.find(params[:id])
+        @artist.destroy
+        flash[:notice] = "Review deleted."
+        redirect_to animes_path
+      end
+    
+      private
+    
+      def review_params
+        params.require(:review).permit(:rating, :title, :content, :anime_id, :user_id)
+      end
 end
