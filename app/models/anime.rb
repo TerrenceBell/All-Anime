ac9e33326ddece856f180afcb51eb0f92077e5f9
TerrenceBell <terrencekbell@gmail.com>
2020-06-03 22:30:29 -0500

    Adding model scope method for Anime model

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -1,4 +1,7 @@
 class Anime < ApplicationRecord
     has_many :reviews
     has_many :users, through: :reviews
+    scope :horror_genre, -> { where(genre: "Horror")}
+    scope :action_adventure_genre, ->{ where(genre: "Action/Adventure")}
+    scope :psycological_genre, -> { where(genre: "Psycological")}
 end
